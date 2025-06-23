606bc3b9e3e18573cf13ba5bdda54eb980894584
Speanight <ltewithl@gmail.com>
2025-06-23 11:31:43 +0200

    Skeleton of the project

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$method = $_SERVER["REQUEST_METHOD"];
+$uri    = explode("?", $_SERVER["REQUEST_URI"])[0];
+
+switch ($method) {
+    case "GET":
+        if ($uri == "/accueil") require_once("pages/accueil.php");
+        else require_once("pages/accueil.php");
+        break;
+    
+    case "POST":
+        break;
+    
+    case "PUT":
+        break;
+    
+    case "DELETE":
+        break;
+}
