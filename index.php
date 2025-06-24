db31546a4ea8320385d3732a5dc0af9b60f30a5f
Nirij3m <nirina.macon@isen-ouest.yncrea.fr>
2025-06-24 18:27:53 +0200

    finished front for adding boat

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ $uri    = explode("?", $_SERVER["REQUEST_URI"])[0];
 switch ($method) {
     case "GET":
         if ($uri == "/accueil") require_once("pages/accueil.php");
+        if($uri == "/boat") require_once("pages/boat.php");
+
         else require_once("pages/accueil.php");
         break;
     
---
+++ pages/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title> index </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
