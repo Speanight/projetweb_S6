bf42ea0d18fabcba05e03515127bc2505cc2d264
romane-crt <Romane.carette44@gmail.com>
2025-06-25 09:03:36 +0200

    Accueil

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,2 +1,17 @@
 <?php
-phpinfo();
+require_once("header.php");
+?>
+<body class="h-screen bg-cover bg-no-repeat  backdrop-blur-sm"
+style="background-image: url('assets/bg_accueil.jpg');">
+<div class="bg-center bg-[#E2F3FA]/40 mt-40 flex flex-col items-center justify-center text-center">
+    <h1 class="font-[Roboto] text-[40px] font-semibold text-white mb-10 mt-5"> Goals of our Project </h1>
+    <h2 class="font-[inter] text-2xl font-normal text-white mr-120 ml-120"> After collecting a set of data on ships sailing in the Gulf of Mexico, we cleaned, predicted, and visualized it in many different ways. </h2>
+    <div class="container py-16">
+        <a href="javascript:void(0)"
+        class="bg-[#1B44C8] border-0 rounded-md inline-flex items-center justify-center py-3 px-7 text-center text-base font-medium text-white hover:bg-body-color disabled:bg-gray-3 disabled:text-dark-5">
+        Learn More
+        </a>
+    </div>
+</div>
+</body>
+</html>
---
+++ pages/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title> index </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    </head>
+  <header x-data="
+        {
+          navbarOpen: false
+        }
+      " class="flex w-full items-center dark:bg-dark bg-[#E2F3FA]/40 ">
+    <div class="container mx-auto">
+      <div class="relative -mx-4 flex items-center justify-center">
+        <div class="w-full px-4">
+            <nav :class="!navbarOpen && 'hidden' " id="navbarCollapse"
+              class="flex justify-center items-center w-full rounded-lg bg-white px-6 shadow lg:static lg:block lg:w-full lg:shadow-none dark:bg-dark-2 lg:dark:bg-transparent mt-2 mb-2">
+              <ul class="block lg:flex">
+                <div class ="flex flex-row items-center">
+                  <li>
+                    <a href="javascript:void(0)">
+                    <img src="assets/logo.jpg" alt="logo" class="dark:hidden" width="69" height="69" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:void(0)"
+                    lass="flex py-2 text-lg font-bold text-body-color hover:text-dark lg:ml-12 lg:inline-flex dark:text-dark-6 dark:hover:text-white">
+                    Titan'ISEN
+                    </a>
+                  </li>
+                
+                <li>
+                  <a href="/accueil"
+                    class="flex py-2 text-base font-medium text-body-color hover:text-dark lg:ml-12 lg:inline-flex dark:text-dark-6 dark:hover:text-white">
+                    Home Page
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0)"
+                    class="flex py-2 text-base font-medium text-body-color hover:text-dark lg:ml-12 lg:inline-flex dark:text-dark-6 dark:hover:text-white">
+                    Add your own boat
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0)"
+                    class="flex py-2 text-base font-medium text-body-color hover:text-dark lg:ml-12 lg:inline-flex dark:text-dark-6 dark:hover:text-white">
+                    Tracking
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0)"
+                    class="flex py-2 text-base font-medium text-body-color hover:text-dark lg:ml-12 lg:inline-flex dark:text-dark-6 dark:hover:text-white">
+                    About our Project
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0)"
+                    class="flex py-2 text-base font-medium text-body-color hover:text-dark lg:ml-12 lg:inline-flex dark:text-dark-6 dark:hover:text-white">
+                    <input type="search" placeholder ="Search" class="border rounded-lg px-2 py-1">
+                  </a>
+                </li>
+                </div>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
